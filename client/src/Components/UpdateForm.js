36d73543a63bb730aa1ce23c7b7f628b72d468d9
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-09-09 17:59:36 -0500

    Working on delete request

---
+++ client/src/Components/UpdateForm.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import axios from "axios";
+
+
+
+const initialMovie = {
+    id: "",
+    title: "",
+    director: "",
+    metascore: "", 
+    stars: ""
+}
+
+const UpdateForm = (props) => {
+    const {push} = useHistory();
+    const [movie, setMovie] = useState(initialMovie)
+    const {id} = useParams();
+
+    useEffect(() => {
+        axios
+        .get(`http://localhost:5000/api/movies/${id}`)
+        .then(res => {
+            setMovie(res.data)
+        })
+        .catch(err => console.log(err))
+    }, [id])
+
+    const changeHandler = e => {
+       setMovie({...movie, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios
+        .put(`http://localhost:5000/api/movies/${id}`, movie)
+        .then(res => {
+            console.log(res)
+            // props.setMovieList(res)
+            // UNCOMMENT ABOVE FOR WEIRD MAP ISSUE. PAGE DOESN'T REFRESH ON ITS OWN. 
+            push("/")
+        })
+        .catch(err => console.log(err))
+    }
+
+    
+    return(
+        <div>
+            <h2>Update Movie</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="number"
+                    name="id"
+                    onChange={changeHandler}
+                    placeholder="id"
+                    value={movie.id}/>
+                <input
+                    type="text"
+                    name="title"
+                    onChange={changeHandler}
+                    placeholder="title"
+                    value={movie.title}/>
+                <input
+                    type="text"
+                    name="director"
+                    onChange={changeHandler}
+                    placeholder="director"
+                    value={movie.director}/>
+                <input
+                    type="number"
+                    name="metascore"
+                    onChange={changeHandler}
+                    placeholder="metascore"
+                    value={movie.metascore}/>
+                <input
+                    type="text"
+                    name="stars"
+                    onChange={changeHandler}
+                    placeholder="stars"
+                    value={movie.stars}/>
+                <button type="submit">Update Movie</button>
+            </form>
+            
+        </div>
+    )
+}
+
+export default UpdateForm;
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie({ addToSavedList }, props) {
   const [movie, setMovie] = useState(null);
-  const params = useParams();
+  const { push } = useHistory();
+  const { params } = useParams();
 
   const fetchMovie = (id) => {
     axios
@@ -13,6 +14,17 @@ function Movie({ addToSavedList }) {
       .then((res) => setMovie(res.data))
       .catch((err) => console.log(err.response));
   };
+
+  // const deleteMovie = (e) => {
+  //   e.preventDefault()
+  //   axios
+  //   .delete(`http://localhost:5000/api/movies/`)
+  //   .then(res => {
+  //     console.log(res)
+  //     push("/")
+  //   })
+  //   .catch(err => console.log(err))
+  // }
 
   const saveMovie = () => {
     addToSavedList(movie);
@@ -33,6 +45,10 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <button onClick={() => push(`/update-movie/${movie.id}`)}>
+        Edit Movie
+      </button>
+      <button>Delete Movie</button>
     </div>
   );
 }
